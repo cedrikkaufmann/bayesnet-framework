72b670590a812eef8304b2fc54d6273f449bc7bb
ckaufmann <cedrik.kaufmann@gmail.com>
2019-06-06 16:58:17 +0200

    add factor implementation

---
+++ include/bayesnet/factor.h
@@ -0,0 +1,37 @@
+//
+// Created by Cedrik Kaufmann on 2019-05-31.
+//
+
+#ifndef BAYESNET_FRAMEWORK_FACTOR_H
+#define BAYESNET_FRAMEWORK_FACTOR_H
+
+#include <dai/factor.h>
+#include <bayesnet/factor.h>
+
+namespace BayesNet {
+
+    class Factor : public dai::Factor {
+    public:
+        Factor() : dai::Factor(), _isEvidence(false) {}
+        explicit Factor(dai::VarSet &vars) : dai::Factor(vars), _isEvidence(false), _backupFactor(vars.nrStates().get_ui()) {}
+
+        void setEvidence(size_t state);
+
+        void clearEvidence();
+
+    private:
+        bool _isEvidence;
+        std::vector<double> _backupFactor;
+
+        void backup();
+
+        void restore();
+    };
+
+    class GaussianFactor : public dai::Factor {
+    public:
+        explicit GaussianFactor(dai::VarSet &vars) : dai::Factor(vars) {};
+    };
+}
+
+#endif //BAYESNET_FRAMEWORK_FACTOR_H
---
+++ src/factor.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Cedrik Kaufmann on 2019-05-31.
+//
+
+#include <bayesnet/factor.h>
+#include <bayesnet/state.h>
+
+void BayesNet::Factor::setEvidence(size_t state) {
+    if (this->_isEvidence) {
+        this->restore();
+    }
+
+    this->backup();
+
+    size_t jointStates = this->nrStates();
+    size_t evidenceEntries = jointStates / BAYESNET_STATES;
+    size_t evidenceBeginIndex = evidenceEntries * state;
+    size_t evidenceEndIndex = evidenceBeginIndex + evidenceEntries;
+
+    for (size_t i = 0; i < jointStates; i++) {
+
+        if (i < evidenceBeginIndex || i >= evidenceEndIndex) {
+            this->set(i, 0);
+        }
+    }
+
+    this->_isEvidence = true;
+}
+
+void BayesNet::Factor::clearEvidence() {
+    this->restore();
+    this->_isEvidence = false;
+}
+
+void BayesNet::Factor::backup() {
+    for (int j = 0; j < this->nrStates(); ++j) {
+        this->_backupFactor.at(j) = this->get(j);
+    }
+}
+
+void BayesNet::Factor::restore() {
+    for (int j = 0; j < this->nrStates(); ++j) {
+        this->set(j, this->_backupFactor.at(j));
+    }
+}
