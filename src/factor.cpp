7699fc47807b98c6861b669336f2c621a054c4ec
ckaufmann <cedrik.kaufmann@gmail.com>
2019-06-06 17:50:34 +0200

    add namespace

--- src/factor.cpp
+++ src/factor.cpp
@@ -5,41 +5,45 @@
 #include <bayesnet/factor.h>
 #include <bayesnet/state.h>
 
-void BayesNet::Factor::setEvidence(size_t state) {
-    if (this->_isEvidence) {
-        this->restore();
+namespace BayesNet {
+
+    void Factor::setEvidence(size_t state) {
+        if (this->_isEvidence) {
+            this->restore();
+        }
+
+        this->backup();
+
+        size_t jointStates = this->nrStates();
+        size_t evidenceEntries = jointStates / BAYESNET_STATES;
+        size_t evidenceBeginIndex = evidenceEntries * state;
+        size_t evidenceEndIndex = evidenceBeginIndex + evidenceEntries;
+
+        for (size_t i = 0; i < jointStates; i++) {
+
+            if (i < evidenceBeginIndex || i >= evidenceEndIndex) {
+                this->set(i, 0);
+            }
+        }
+
+        this->_isEvidence = true;
     }
 
-    this->backup();
+    void Factor::clearEvidence() {
+        this->restore();
+        this->_isEvidence = false;
+    }
 
-    size_t jointStates = this->nrStates();
-    size_t evidenceEntries = jointStates / BAYESNET_STATES;
-    size_t evidenceBeginIndex = evidenceEntries * state;
-    size_t evidenceEndIndex = evidenceBeginIndex + evidenceEntries;
-
-    for (size_t i = 0; i < jointStates; i++) {
-
-        if (i < evidenceBeginIndex || i >= evidenceEndIndex) {
-            this->set(i, 0);
+    void Factor::backup() {
+        for (int j = 0; j < this->nrStates(); ++j) {
+            this->_backupFactor.at(j) = this->get(j);
         }
     }
 
-    this->_isEvidence = true;
-}
-
-void BayesNet::Factor::clearEvidence() {
-    this->restore();
-    this->_isEvidence = false;
-}
-
-void BayesNet::Factor::backup() {
-    for (int j = 0; j < this->nrStates(); ++j) {
-        this->_backupFactor.at(j) = this->get(j);
+    void Factor::restore() {
+        for (int j = 0; j < this->nrStates(); ++j) {
+            this->set(j, this->_backupFactor.at(j));
+        }
     }
-}
 
-void BayesNet::Factor::restore() {
-    for (int j = 0; j < this->nrStates(); ++j) {
-        this->set(j, this->_backupFactor.at(j));
-    }
 }
